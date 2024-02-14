5eebcfc8de3c26e3f1f388eb88ce50c389812034
Kevin Liao <kevin.liao@kellogg.northwestern.edu>
2024-02-14 09:56:00 +0000

    20220214

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  resources "places"
+  resources "journals"
   # get("/", { :controller => "articles", :action => "index" })
 end
