ec1b0296fdf17ee500af8eb844ac4fc1ca5952d1
Kevin Liao <kevin.liao@kellogg.northwestern.edu>
2024-02-18 23:46:04 +0000

    Progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  def index
+    # find all Company rows
+    @companies = Company.all
+    # render companies/index view
+  end
+
+  def show
+    # find a Company
+    @company = Company.find_by({"id" => params["id"]})
+    # render companies/show view with details about Company
+  end
+
+  def new
+    # render view with new Company form
+  end
+
+  def create
+    # start with a new Company
+    @company = Company.new
+    # assign user-entered form data to Company's columns
+    @company["name"] = params["name"]
+    @company["city"] = params["city"]
+    @company["state"] = params["state"]
+    # save Company row
+    @company.save
+    # redirect user
+    redirect_to "/companies"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  resources "entries"
   resources "places"
-  resources "journals"
   # get("/", { :controller => "articles", :action => "index" })
 end
